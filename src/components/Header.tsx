4befae895a3bfe1c2d9b5a8ea67b5bd0506bba3d
nakagawa <nklll.0229@gmail.com>
2025-01-17 11:28:27 +0900

    仮置きでログインとログアウト、マイページなどの共同が変更できるようになってます

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,41 @@
 import Link from "next/link";
 import Styles from "@/styles/Home.module.css";
-import useSWR from "swr";
+import { text } from "stream/consumers";
+import { useState } from "react";
 
 export default function Header() {
+  const [text, setText] = useState(true);
   //   const {
   //     data: res,
   //     error,
   //     isLoading,
-  //   } = useSWR("フェッチAPI。クッキー持ってるかの認証", (url: string) => fetch(url));
+  //   } = useSWR("フェッチAPI。認証", (url: string) => fetch(url));
 
   //   const loginStatus = !error && !isLoading && res && res.status === 200;
   //API確認次第、コメントの部分を表示させます！
+
+  const handleClick = () => {
+    setText(!text);
+  };
+
   return (
     <div className={Styles.header}>
       <div>
-        <p>
-          <Link href="/login">ログイン</Link>
-        </p>
-        <p>
-          <Link href="/logout">ログアウト</Link>
-        </p>
+        {text && (
+          <p>
+            <a href="#" onClick={handleClick}>
+              ログイン
+            </a>
+          </p>
+        )}
+        {!text && (
+          <p>
+            <a href="#" onClick={handleClick}>
+              ログアウト
+            </a>
+          </p>
+        )}
+
         {/* {loginStatus&&(
            <p>
              <Link href="/login">ログイン</Link>
@@ -38,9 +54,19 @@ export default function Header() {
         <p>
           <Link href="/list">投稿一覧</Link>
         </p>
-        <p>
+        {/* {loginStatus&&(
+           <p>
           <Link href="/myPage">マイページ</Link>
-        </p>
+           </p>
+        )}
+       */}
+        {!text && (
+          <p>
+            <a href="#" onClick={handleClick}>
+              マイページ
+            </a>
+          </p>
+        )}
       </div>
     </div>
   );
