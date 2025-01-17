6183e7db93c43d953ef39e00a041ac3eb093c725
asanuma <haruno.asanuma@rakus-partners.co.jp>
2025-01-17 15:28:50 +0900

    link用global css変更、ログイン画面作成中

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,20 @@
 import Link from "next/link";
+import { useState } from "react";
+import Layout from "@/components/layout/Layout";
+import Form from "@/components/Form";
+import Input from "@/components/Input";
+import Button from "@/components/Button";
 import liginStyle from "@/styles/Login.module.css";
+import LayoutStyle from "../../components/layout/layout.module.css";
 
 export default function Login() {
+  // メールアドレス、パスワード初期値
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // 「ログイン」を押下した時
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
   return (
     <>
       <Layout
@@ -10,23 +23,30 @@ export default function Login() {
         headContent="rahaログインページ"
         pageTitle="ログイン"
       >
-        {/* タイトルコンポーネント */}
-        <h1>ログイン</h1>
-        {/* フォームコンポーネント */}
-        <form action="" method="POST">
+        <Form handleSubmit={handleSubmit}>
           {/* inputコンポーネント？ */}
-          <label htmlFor="email">
-            メールアドレス
-            <input type="text" id="email" value={""} />
-          </label>
-          <label htmlFor="password">
-            パスワード
-            <input type="password" id="password" value={""} />
-          </label>
-          <Link href="/">ユーザー登録はこちら</Link>
-          {/* ボタンコンポーネント */}
-          <button type="submit">ログイン</button>
-        </form>
+          <Input
+            label={"メールアドレス"}
+            type={"email"}
+            inputId={"email"}
+            inputName={"email"}
+            value={email}
+            handleChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            label={"パスワード"}
+            type={"password"}
+            inputId={"password"}
+            inputName={"password"}
+            value={password}
+            handleChange={(e) => setPassword(e.target.value)}
+          />
+          <Link href="/" className="link">
+            ユーザー登録はこちら
+          </Link>
+
+          <Button type={"submit"} buttonText={"ログイン"} />
+        </Form>
       </Layout>
     </>
   );
