0789cff3367fc390201bc2fe8e646a3419471a46
asanuma <haruno.asanuma@rakus-partners.co.jp>
2025-01-21 10:23:55 +0900

    エラーではない場合、トップページに遷移するコードを追加

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import { useRouter } from "next/router";
 import Layout from "@/components/layout/Layout";
 import Form from "@/components/form/Form";
 import Input from "@/components/input/Input";
 import Button from "@/components/button/Button";
 import inputStyle from "@/components/input/input.module.css";
-import loginStyle from "@/styles/Login.module.css";
 import ColorLink from "@/components/ColorLink/ColorLink";
+import loginStyle from "@/styles/Login.module.css";
 
 export default function Login() {
   // メールアドレス、パスワード 初期値
@@ -20,24 +21,35 @@ export default function Login() {
   const [emailError, setEmailError] = useState("");
   const [passError, setPassError] = useState("");
 
+  // 画面遷移
+  const [isValid, setIsValid] = useState(true);
+  const router = useRouter();
+
   // 「ログイン」を押下した時
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    let hasError = false;
+
     if (email.trim() === "") {
-      e.preventDefault();
       setEmailError("メールアドレスを入力してください");
       setInputEmailArea(inputStyle.errorInput);
+      hasError = true;
     } else {
       setEmailError("");
       setInputEmailArea(inputStyle.usualInput);
     }
 
     if (password.trim() === "") {
-      e.preventDefault();
       setPassError("パスワードを入力してください");
       setInputPassArea(inputStyle.errorInput);
+      hasError = true;
     } else {
       setPassError("");
       setInputPassArea(inputStyle.usualInput);
+    }
+    setIsValid(!hasError);
+    if (!hasError) {
+      router.push("/");
     }
   };
 
@@ -49,42 +61,34 @@ export default function Login() {
         headContent="rahaログインページ"
         pageTitle="ログイン"
       >
-        <Form
-          action=""
-          method="POST"
-          handleSubmit={handleSubmit}
-          noValidate={true}
-        >
+        <Form method="POST" handleSubmit={handleSubmit} noValidate={true}>
           <Input
-            label={"メールアドレス"}
-            type={"email"}
-            inputId={"email"}
-            placeholder={"メールアドレスを入力してください"}
-            inputName={"email"}
+            label="メールアドレス"
+            type="email"
+            inputId="email"
+            placeholder="メールアドレスを入力してください"
+            inputName="email"
             value={email}
             handleChange={(e) => setEmail(e.target.value)}
             inputClass={inputEmailArea}
             errorMessage={emailError}
           />
           <Input
-            label={"パスワード"}
-            type={"password"}
-            inputId={"password"}
-            placeholder={"パスワードを入力してください"}
-            inputName={"password"}
+            label="パスワード"
+            type="password"
+            inputId="password"
+            placeholder="パスワードを入力してください"
+            inputName="password"
             value={password}
             handleChange={(e) => setPassword(e.target.value)}
             inputClass={inputPassArea}
             errorMessage={passError}
           />
           <div className={loginStyle.linkWrap}>
-            <ColorLink
-              colorLinkText={"ユーザー登録はこちら"}
-              url={"/register"}
-            />
+            <ColorLink colorLinkText="ユーザー登録はこちら" url="/register" />
           </div>
           <div className={loginStyle.btnWrap}>
-            <Button type={"submit"} buttonText={"ログイン"} size={"M"} />
+            <Button type="submit" buttonText="ログイン" size="M" />
           </div>
         </Form>
       </Layout>
