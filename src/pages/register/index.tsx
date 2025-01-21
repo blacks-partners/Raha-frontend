49bee76ba944e66f90302fe2ff0901a50bbf6be1
asanuma <haruno.asanuma@rakus-partners.co.jp>
2025-01-21 10:58:30 +0900

    画面に必要な情報、CSSの適用

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,8 +1,32 @@
 import Form from "@/components/form/Form";
 import Input from "@/components/input/Input";
 import Layout from "@/components/layout/Layout";
+import { useState } from "react";
+import inputStyle from "@/components/input/input.module.css";
+import Button from "@/components/button/Button";
+import registerStyle from "@/styles/Register.module.css";
 
 export default function Register() {
+  // 新規登録　初期値
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setpassword2] = useState("");
+  const [introduction, setIntroduction] = useState("");
+
+  // 入力欄 初期値
+  const [inputNameArea, setInputNameArea] = useState(inputStyle.usualInput);
+  const [inputEmailArea, setInputEmailArea] = useState(inputStyle.usualInput);
+  const [inputPassArea, setInputPassArea] = useState(inputStyle.usualInput);
+  const [inputPass2Area, setInputPass2Area] = useState(inputStyle.usualInput);
+  const [textArea, setTextArea] = useState(inputStyle.usualInput);
+
+  // エラー 初期値
+  const [inputNameError, setInputNameError] = useState("");
+  const [inputEmailError, setInputEmailError] = useState("");
+  const [inputPassError, setInputPassError] = useState("");
+  const [inputPass2Error, setInputPass2Error] = useState("");
+
   const handleSubmit = () => {
     console.log("aaa");
   };
@@ -16,15 +40,60 @@ export default function Register() {
       >
         <Form handleSubmit={handleSubmit}>
           <Input
-            label={"名前"}
-            type={"text"}
-            inputId={"name"}
-            inputName={"name"}
-            inputClass={""}
-            errorMessage=""
-            value={""}
-            handleChange={""}
+            label="名前"
+            type="text"
+            inputId="name"
+            inputName="name"
+            inputClass={inputNameArea}
+            errorMessage={inputNameError}
+            value={name}
+            handleChange={(e) => setName(e.target.value)}
+            placeholder="例）山田太郎"
           />
+          <Input
+            label="メールアドレス"
+            type="email"
+            inputId="email"
+            inputName="email"
+            inputClass={inputNameArea}
+            errorMessage={inputEmailError}
+            value={email}
+            handleChange={(e) => setEmail(e.target.value)}
+            placeholder="例）example@example.com"
+          />
+          <Input
+            label="パスワード"
+            type="password"
+            inputId="password"
+            inputName="password"
+            inputClass={inputPassArea}
+            errorMessage={inputPassError}
+            value={password}
+            handleChange={(e) => setPassword(e.target.value)}
+            placeholder="8文字以上16文字以内"
+          />
+          <Input
+            label="確認用パスワード"
+            type="password"
+            inputId="password2"
+            inputName="password2"
+            inputClass={inputPass2Area}
+            errorMessage={inputPass2Error}
+            value={password2}
+            handleChange={(e) => setpassword2(e.target.value)}
+            placeholder="再度パスワードを入力"
+          />
+          <div className={registerStyle.textAreaWrap}>
+            <label htmlFor="introduction">自己紹介</label>
+            <textarea
+              name="introduction"
+              id="introduction"
+              placeholder="例）エンジニア初心者です"
+            ></textarea>
+          </div>
+          <div className={registerStyle.btnWrap}>
+            <Button type="button" buttonText="登録" size="M" />
+          </div>
         </Form>
       </Layout>
     </>
