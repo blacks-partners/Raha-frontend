7577993721dc59e0f8bf37db156fa4603e7adc77
nakagawa <nklll.0229@gmail.com>
2025-01-24 11:50:16 +0900

    新規投稿ページの作成

---
+++ src/pages/new_post/index.tsx
@@ -0,0 +1,34 @@
+import Layout from "@/components/layout/Layout";
+import MarkDown from "@/components/markDown/MarkDown";
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+export default function NewPost() {
+  const router = useRouter();
+
+  const [postTitle, setPostTitle] = useState("");
+  const [postTextarea, SetPostTextarea] = useState("");
+
+  const handlePost = () => {
+    // ここでAPIリクエストなど実行してもOK
+    // 最終的にページ遷移:
+    router.push("/post_details/1");
+  };
+  return (
+    <Layout
+      headContent="新規投稿"
+      headName="新規投稿"
+      headTitle="新規投稿"
+      pageTitle="新規投稿"
+    >
+      <MarkDown
+        buttonText="投稿"
+        textarea={postTextarea}
+        onContentChange={SetPostTextarea}
+        title={postTitle}
+        onTitleChange={setPostTitle}
+        onPost={handlePost}
+      ></MarkDown>
+    </Layout>
+  );
+}
