2fc5cc0e4e432b331a1d76327e999ee8bed712aa
aokihinako <tomatoro0422@gmail.com>
2025-01-30 16:11:17 +0900

    投稿編集画面修正

--- src/pages/edit_post/[id].tsx
+++ src/pages/edit_post/[id].tsx
@@ -4,15 +4,16 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 import { GetServerSideProps } from "next";
 
-interface EdidPostProps {
+interface EditPostProps {
   postData: {
     title: string;
     content: string;
     articleId: number;
   };
+  token: string;
 }
 
-export default function EditPost({ postData }: EdidPostProps) {
+export default function EditPost({ postData, token }: EditPostProps) {
   const router = useRouter();
   console.log(postData);
   console.log(postData.title);
@@ -23,24 +24,35 @@ export default function EditPost({ postData }: EdidPostProps) {
 
   // 「投稿」ボタン押下時に呼ばれる
   const handlePost = async () => {
-    console.log("hoge");
-    console.log(postData);
+    console.log(
+      "getServerSidePropsで取得したpostDataがわたってきてるか確認",
+      postData
+    );
+    console.log(
+      "getServerSidePropsで取得したtoken(JWT)がわたってきてるか確認",
+      token
+    );
 
     // PUTリクエストでデータを更新
     const response = await fetch(
       `http://localhost:8080/articles/${postData.articleId}`,
       {
+        //API設計書の通りのmethod
+
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
-          "X-AUTH-TOKEN":
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImV4cCI6MTc0MDgwNTY2Nn0.c9iu6gUqHs__7XB8MROtjhUwzzIYOjxI4hPeoevX0VY",
+          //headersに渡されたtokenを格納
+          // "X-AUTH-TOKEN": token,
         },
+        //bodyに含めれたtitleとcontentをJSON文字列に変換
         body: JSON.stringify({ title, content }),
       }
     );
-    console.log("hogehoge");
-    console.log(response);
+    console.log(
+      "変数responseにparamsと同じ記事データが格納されたか確認",
+      response
+    );
 
     if (response.ok) {
       // 更新が成功したら該当する投稿詳細画面へ遷移
@@ -75,6 +87,15 @@ export default function EditPost({ postData }: EdidPostProps) {
 export const getServerSideProps: GetServerSideProps = async (context) => {
   // context.paramsからidを取得
   const { id } = context.params as { id: string };
+  //リクエスト情報からcookiesを取得
+  const userCookie = context.req.cookies;
+  console.log("cookies内の情報の閲覧", userCookie);
+
+  //クッキーの中のトークンを取得
+  // const token = userCookie.token;
+  // console.log("tokenの閲覧", token);
+
+  //params.idと同じidをもつ記事データのフェッチ
 
   const response = await fetch(`http://localhost:8080/articles/${id}`);
   console.log(response);
@@ -93,6 +114,8 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   return {
     props: {
       postData,
+      // token,
+      //引数に渡すもの
     },
   };
 };
